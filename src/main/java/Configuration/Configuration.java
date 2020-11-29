73603d76afbb874ed005d34ed2a2c1b340c52846
Flash3r01 <tim-johmann@web.de>
2020-11-29 19:14:07 +0100

    removed unnecessary config fields

--- src/main/java/Configuration/Configuration.java
+++ src/main/java/Configuration/Configuration.java
@@ -20,10 +20,6 @@ public enum Configuration {
     public String[] forbiddenItems = {"kn!fe", "glock|7", "exl|os!ve"};
     public String key = "FranzJosefStrauß";
     public String searchAlgorithm = "BoyerMoore";
-    public ProfileType[] noFunctionalityAllowed = {ProfileType.K, ProfileType.O};
-    public ProfileType[] functionalityOneTroughFour = {ProfileType.I};
-    public ProfileType[] functionalityFive = {ProfileType.S};
-    public ProfileType[] functionalitySix = {ProfileType.T};
     //todo does not work public ISearchAlgorithm activeSearchAlgorithm = BoyerMoore;
 
 
